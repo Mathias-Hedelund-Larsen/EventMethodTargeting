b444a614edebbb9abd3c8246f19529b26778a92f
MathDodo <mathias.h.larsen@jp.dk>
2020-03-12 20:37:33 +0100

    Assembly search shortened

--- Assets/FoldergeistAssets/Editor/UnityEventMethodTargeting/PropertyDrawers/TargetMethodDataPropertyDrawer.cs
+++ Assets/FoldergeistAssets/Editor/UnityEventMethodTargeting/PropertyDrawers/TargetMethodDataPropertyDrawer.cs
@@ -142,8 +142,7 @@ namespace FoldergeistAssets
                     List<string> typeNames = new List<string>();
                     List<string> displayTypeNames = new List<string>();
 
-                    var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.FullName.Contains("Editor") && !a.FullName.Contains("firstpass")).
-                    Where(a => a.FullName.Contains("Assembly-CSharp") || a.FullName.Contains("FoldergeistAssets")).ToList();
+                    var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(a => !a.FullName.Contains("Editor") && !a.FullName.Contains("firstpass")).ToList();
 
                     for (int i = 0; i < assemblies.Count; i++)
                     {
