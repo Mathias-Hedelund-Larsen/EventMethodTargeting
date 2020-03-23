97d624c645b58c183ff5a09266a85d5d336bf47a
MathDodo <mathias.h.larsen@jp.dk>
2020-03-23 11:45:56 +0100

    23-03-2020 11:45:56

--- Assets/HephaestusForge/Editor/UnityEventMethodTargeting/PropertyDrawers/NewUnityEventAttributePropertyDrawer.cs
+++ Assets/HephaestusForge/Editor/UnityEventMethodTargeting/PropertyDrawers/NewUnityEventAttributePropertyDrawer.cs
@@ -8,29 +8,71 @@ using UnityEngine.Events;
 
 namespace HephaestusForge.UnityEventMethodTargeting
 {
+    [CustomPropertyDrawer(typeof(EventMethodTargetAttribute))]
     public class NewUnityEventAttributePropertyDrawer : PropertyDrawer
     {
+        private string _propertyName;
         private string _propertyPath;
         private Dictionary<string, ReorderableList> _initialized = new Dictionary<string, ReorderableList>();
 
         private ReorderableList Init(SerializedProperty property)
-        {
+        {            
             Selection.selectionChanged -= OnLostInspectorFocus;
             Selection.selectionChanged += OnLostInspectorFocus;
+
+            var list = new ReorderableList(property.serializedObject, property.FindPropertyRelative("m_PersistentCalls").FindPropertyRelative("m_Calls"));
+
+            list.drawHeaderCallback = DrawHeader;
+            list.drawElementCallback = DrawListElement;
+            list.onAddDropdownCallback = OnAddClicked;
+            list.onRemoveCallback = OnRemoveClicked;
+
+            return list;
+        }
+
+        private void DrawHeader(Rect rect)
+        {
+            GUI.enabled = false;
+            EditorGUI.LabelField(rect, _propertyName);
+            GUI.enabled = true;
+        }
+
+        private void DrawListElement(Rect rect, int index, bool isActive, bool isFocused)
+        {
+            var persistantCallProperty = _initialized[_propertyPath].serializedProperty.GetArrayElementAtIndex(index);
+
+            var targetProperty = persistantCallProperty.FindPropertyRelative("m_Target");
+            var methodNameProperty = persistantCallProperty.FindPropertyRelative("m_MethodName");
+            var listenerModeProperty = persistantCallProperty.FindPropertyRelative("m_Mode");
+            var callStateProperty = persistantCallProperty.FindPropertyRelative("m_CallState");
+            var argumentsProperty = persistantCallProperty.FindPropertyRelative("m_Arguments ");
+
+            rect.height = EditorGUIUtility.singleLineHeight;
+
+            rect.width = rect.width / 3 - 5;
+
+            EditorGUI.PropertyField(rect, callStateProperty);
+        }
+
+        private void OnAddClicked(Rect buttonRect, ReorderableList list)
+        { 
+        }
+
+        private void OnRemoveClicked(ReorderableList list)
+        {
         }
 
         public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
         {
+            _propertyName = property.displayName;
             _propertyPath = property.propertyPath;
-
-            if (!_initialized.ContainsKey(_propertyPath))
-            {
-                _initialized.Add(_propertyPath, Init(property));
-            }
-
+           
             if (FieldTypeIsUnityEvent())
             {
-
+                if (!_initialized.ContainsKey(_propertyPath))
+                {
+                    _initialized.Add(_propertyPath, Init(property));
+                }
             }
 
             return 0;
@@ -38,16 +80,17 @@ namespace HephaestusForge.UnityEventMethodTargeting
 
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
-            _propertyPath = property.propertyPath;
-
-            if (!_initialized.ContainsKey(_propertyPath))
-            {
-                _initialized.Add(_propertyPath, Init(property));
-            }
+            _propertyName = property.displayName;
+            _propertyPath = property.propertyPath;            
 
             if (FieldTypeIsUnityEvent())
             {
+                if (!_initialized.ContainsKey(_propertyPath))
+                {
+                    _initialized.Add(_propertyPath, Init(property));
+                }
 
+                _initialized[_propertyPath].DoLayoutList();
             }
         }
 
